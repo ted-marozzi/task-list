9d025869d7e2f7ee6e5313a30476a6c37f9053d6
Ted Marozzi <edwardmarozzi@gmail.com>
2024-01-10 22:11:54 +1100

    Update build configuration and ignore main.js file in repository

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,35 @@
-import { MarkdownView, Plugin } from "obsidian";
+import { MarkdownView, Plugin, type Command } from "obsidian";
 import { remark } from "remark";
 import type { Root } from "mdast";
 import { visit } from "unist-util-visit";
 import remarkGfm from "remark-gfm";
 
 export default class TaskList extends Plugin {
+	sortListCommand: Command = {
+		id: "sort-list",
+		name: "Sort list",
+		editorCallback: async (editor) => {
+			const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+
+			if (view === null) {
+				this.log(
+					"info",
+					"Unable to Sort lists as the current file is not a markdown file."
+				);
+				return;
+			}
+
+			const originalMarkdown = editor.getDoc().getValue();
+
+			const modifiedMarkdownFile = await remark()
+				.use(remarkGfm)
+				.use(this.sortTasks)
+				.process(originalMarkdown);
+
+			editor.setValue(modifiedMarkdownFile.toString());
+		},
+	};
+
 	log(level: "info" | "warn" | "error", ...messages: Array<unknown>) {
 		const prefix = `[${this.manifest.name}]`;
 		switch (level) {
@@ -21,45 +46,25 @@ export default class TaskList extends Plugin {
 	}
 
 	async onload() {
-		this.addCommand({
-			id: "sort-list",
-			name: "Sort list",
-			editorCallback: async (editor) => {
-				const view =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
+		this.addCommand(this.sortListCommand);
+	}
 
-				if (view === null) {
-					this.log(
-						"info",
-						"Unable to Sort lists as the current file is not a markdown file."
-					);
-					return;
-				}
+	sortTasks() {
+		return (mdast: Root) => {
+			visit(mdast, "list", (node) => {
+				node.children.sort((a, b) => {
+					if (a.checked === true && !b.checked) {
+						return 1;
+					}
 
-				const originalMarkdown = editor.getDoc().getValue();
+					if (b.checked === true && !a.checked) {
+						return -1;
+					}
 
-				const modifiedMarkdownFile = await remark()
-					.use(remarkGfm)
-					.use(() => (mdast: Root) => {
-						visit(mdast, "list", (node) => {
-							node.children.sort((a, b) => {
-								if (a.checked === true && !b.checked) {
-									return 1;
-								}
-
-								if (b.checked === true && !a.checked) {
-									return -1;
-								}
-
-								return 0;
-							});
-						});
-					})
-					.process(originalMarkdown);
-
-				editor.setValue(modifiedMarkdownFile.toString());
-			},
-		});
+					return 0;
+				});
+			});
+		};
 	}
 
 	onunload() {}
