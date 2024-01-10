a5a5a31cc4085153704a238d3b3254df3100ab30
Ted Marozzi <edwardmarozzi@gmail.com>
2024-01-10 17:08:50 +1100

    Add dependencies and partially implement list sorting command

--- main.ts
+++ main.ts
@@ -1,7 +1,56 @@
-import { Plugin } from "obsidian";
+import { MarkdownView, Plugin } from "obsidian";
+import { remark } from "remark";
+import type { Root } from "mdast";
+import { visit } from "unist-util-visit";
 
 export default class TaskList extends Plugin {
-	async onload() {}
+	log(level: "info" | "warn" | "error", ...messages: Array<unknown>) {
+		const prefix = `[${this.manifest.name}]`;
+		switch (level) {
+			case "info":
+				console.log(prefix, ...messages);
+				break;
+			case "warn":
+				console.warn(prefix, ...messages);
+				break;
+			case "error":
+				console.error(prefix, ...messages);
+				break;
+		}
+		console.info(messages);
+	}
+
+	async onload() {
+		this.addCommand({
+			id: "sort-list",
+			name: "Sort list",
+			editorCallback: async (editor) => {
+				editor.listSelections;
+				const view =
+					this.app.workspace.getActiveViewOfType(MarkdownView);
+
+				if (view === null) {
+					this.log(
+						"info",
+						"Unable to Sort lists as the current file is not a markdown file."
+					);
+					return;
+				}
+
+				const originalMarkdown = editor.getDoc().getValue();
+
+				const modifiedMarkdownFile = await remark()
+					.use(() => (mdast: Root) => {
+						visit(mdast, "list", (node) => {
+							console.log(node);
+						});
+					})
+					.process(originalMarkdown);
+
+				editor.setValue(modifiedMarkdownFile.toString());
+			},
+		});
+	}
 
 	onunload() {}
 }
